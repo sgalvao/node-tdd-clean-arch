252e086c6f6e19b009769fa9e0b7fca6f87189b8
Silvao <silvio.gabrielgt2000@hotmail.com>
2022-06-24 01:06:56 -0300

    add eslint

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/tests',
+    '<rootDir>/src'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+
+}
